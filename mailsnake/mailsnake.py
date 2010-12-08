dbdb5af6f7e291f95f7455bf33ace4392464b12a
John-Kim Murphy <leftium@gmail.com>
2010-12-08 18:02:55 +0900

    Initial commit

---
+++ mailsnake/mailsnake.py
@@ -0,0 +1,36 @@
+import urllib, urllib2
+import json
+
+class MailSnake(object):
+    def __init__(self, apikey = '', extra_params = {}):
+        """
+            Cache API key and address.
+        """
+        self.apikey = apikey
+
+        self.default_params = {'apikey':apikey}
+        self.default_params.update(extra_params)
+
+        dc = 'us1'
+        if '-' in self.apikey:
+            dc = self.apikey.split('-')[1]
+        self.base_api_url = 'https://%s.api.mailchimp.com/1.3/?method=' % dc
+
+    def call(self, method, params = {}):
+        url = self.base_api_url + method
+        params.update(self.default_params)
+
+        post_data = urllib.urlencode(params)
+        response = urllib2.urlopen(url, post_data)
+
+        return json.loads(response.read())
+
+    def __getattr__(self, method_name):
+
+        def get(self, *args, **kwargs):
+            params = dict((i,j) for (i,j) in enumerate(args))
+            params.update(kwargs)
+            return self.call(method_name, params)
+
+        return get.__get__(self)
+
